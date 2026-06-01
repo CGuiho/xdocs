735bc99ffddf178f3853293080bb30cec46d2e80
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-06-01 22:30:54 +0200

    chore: add xdocs/source/guiho-xdocs-bin.ts

---
+++ xdocs/source/guiho-xdocs-bin.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+/**
+ * @copyright Copyright (c) 2026 GUIHO Technologies as represented by Cristóvão GUIHO. All Rights Reserved.
+ */
+
+import { runMirrorCli } from './guiho-xdocs.js'
+
+await runMirrorCli()
