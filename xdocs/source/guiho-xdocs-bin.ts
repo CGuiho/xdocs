a1d8278944e34344cd24d6aaba65dc46036c7a7b
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-06-02 21:53:57 +0200

    update guiho-xdocs-bin.ts: call runCliWithErrorHandling

--- xdocs/source/guiho-xdocs-bin.ts
+++ xdocs/source/guiho-xdocs-bin.ts
@@ -3,6 +3,6 @@
  * @copyright Copyright (c) 2026 GUIHO Technologies as represented by Cristóvão GUIHO. All Rights Reserved.
  */
 
-import { runMirrorCli } from './guiho-xdocs.js'
+import { runCliWithErrorHandling } from './guiho-xdocs.js'
 
-await runMirrorCli()
+await runCliWithErrorHandling()
