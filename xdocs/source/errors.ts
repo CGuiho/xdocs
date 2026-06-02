5834fa279f28252099d4228811ebffd68024dd36
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-06-02 21:52:57 +0200

    rewrite errors.ts: XDocsError class and invariant

--- xdocs/source/errors.ts
+++ xdocs/source/errors.ts
@@ -2,16 +2,16 @@
  * @copyright Copyright (c) 2026 GUIHO Technologies as represented by Cristóvão GUIHO. All Rights Reserved.
  */
 
-export class MirrorError extends Error {
+export class XDocsError extends Error {
   readonly exitCode: number
 
   constructor(message: string, exitCode = 1) {
     super(message)
-    this.name = 'MirrorError'
+    this.name = 'XDocsError'
     this.exitCode = exitCode
   }
 }
 
 export const invariant = (condition: unknown, message: string): asserts condition => {
-  if (!condition) throw new MirrorError(message)
+  if (!condition) throw new XDocsError(message)
 }
