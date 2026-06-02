2a9efb578b3d08e6affadde84618eb5eab70a5b6
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-06-02 21:53:53 +0200

    rewrite guiho-xdocs.ts: library entrypoint with all exports

--- xdocs/source/guiho-xdocs.ts
+++ xdocs/source/guiho-xdocs.ts
@@ -2,44 +2,52 @@
  * @copyright Copyright (c) 2026 GUIHO Technologies as represented by Cristóvão GUIHO. All Rights Reserved.
  */
 
+// Errors
+export { XDocsError, invariant } from './errors.js'
+
+// Types
 export type {
-  MirrorAdapterName,
-  MirrorCliOptions,
-  MirrorConfig,
-  MirrorExecutionResult,
-  MirrorFormat,
-  MirrorRawConfig,
-  MirrorVersionPlan,
-  MirrorVersionPlanAction,
-  MirrorVersionTarget,
+  XDocsAiMode,
+  XDocsCliOptions,
+  XDocsCommand,
+  XDocsConfig,
+  XDocsFile,
+  XDocsFormat,
+  XDocsMetadata,
+  XDocsParsedArgs,
+  XDocsPromptName,
+  XDocsRawConfig,
+  XDocsScanResult,
+  XDocsTreeNode,
+  XDocsTreeValidation,
 } from './types.js'
 
-export { MirrorError, invariant } from './errors.js'
-export { parseMirrorCliOptions } from './flags.js'
-export { createInitConfig, discoverMirrorConfig, loadMirrorConfig, normalizeMirrorConfig, writeInitConfig } from './config.js'
-export { assertValidSemver, isMirrorReleaseTarget, mirrorReleaseTargets, resolveNextVersion, sortSemverDescending } from './version.js'
+// Flags
+export { parseArgs, stringFlag, booleanFlag, listFlag } from './flags.js'
+
+// Config
 export {
-  createGitCommit,
-  createGitTag,
-  ensureAdapterFiles,
-  ensureGitAvailable,
-  assertSupportedGitTagTemplate,
-  isGitDirty,
-  isGitRepository,
-  readCurrentVersion,
-  readGitVersion,
-  readJsrName,
-  readJsrVersion,
-  readPackageName,
-  readPackageVersion,
-  renderGitTag,
-  resolveProjectName,
-  supportedGitTagTemplates,
-  versionFromTag,
-  writeJsrVersion,
-  writePackageVersion,
-} from './adapters.js'
-export { buildVersionPlan, releaseLabel, resolveFileOutputPaths, validateMirrorConfig } from './plan.js'
-export { applyVersionPlan, executeVersionPlan } from './executor.js'
-export { mirrorBanner, reportConfig, reportConfigSchema, reportExecution, reportExecutionSummary, reportPlan, reportValue } from './reporter.js'
-export { createMirrorCommand, runMirrorCli } from './cli.js'
+  createDefaultConfigContent,
+  defaultConfig,
+  discoverConfig,
+  loadConfig,
+  loadConfigOrDefaults,
+  normalizeConfig,
+  resolvePath,
+  writeDefaultConfig,
+} from './config.js'
+
+// Discovery
+export { isXDocsFile, listDirectoryFiles, scanDirectory, scanProject } from './discovery.js'
+
+// Metadata
+export { extractFrontmatter, parseXDocsFile, validateMetadata } from './metadata.js'
+
+// Tree
+export { buildTree, renderTree, renderTreeMarkdown, validateTree } from './tree.js'
+
+// Help
+export { readPackageVersion, showCommandHelp, showHelp, showVersion } from './help.js'
+
+// CLI
+export { runCli, runCliWithErrorHandling } from './cli.js'
